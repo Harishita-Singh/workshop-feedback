6a7f42fddec57832a5b110bbc382335cfdeb6a85
Your Name <harishitasinghmahua@gmail.com>
2024-10-02 23:59:19 +0530

    output

---
+++ feedback output.py
@@ -0,0 +1,18 @@
+Python 3.12.6 (tags/v3.12.6:a4a2d2b, Sep  6 2024, 20:11:23) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: C:\Users\haris\Desktop\feedback.py =================
+First of all,Thank You from the bottom of my heart Respected Atul Sir!
+
+I`m Harshita B.tech 1st year from CSE(IOT) department and I would like to thank you for such
+a wonderful python workshop session it was, let me say this is one of the most 
+wonderful session I have attended. It`s my first time that I learn python And 
+it`s difficult for me as I don`t even know basics but Thanks to Atul Sir, for
+giving me the knowledge of Python and GitHub also. We all know python is not that 
+easy which can be learned in only 10 days, But now i know the bascis & know how to 
+learn learn any programming language not only python. Sir your teaching in a 
+very excellent way...Not only teaching, you motivates and inspires each and 
+individual student i all aspects always with smiling face ....
+Thank you so mach for spending your valuable time with us.
+
+THANK YOU ATUL SIR! 
