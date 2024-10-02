e741ed8397b925a7f869a362a2941cf9109c82d4
Your Name <harishitasinghmahua@gmail.com>
2024-10-02 23:56:12 +0530

    Feedback

---
+++ feedback.py
@@ -0,0 +1,15 @@
+# Feedback for Python Workshop in GEC, Vaishali.
+print("First of all,Thank You from the bottom of my heart Respected Atul Sir!\n")
+
+print("I`m Harshita B.tech 1st year from CSE(IOT) department and I would like to thank you for such")
+print("a wonderful python workshop session it was, let me say this is one of the most ")
+print("wonderful session I have attended. It`s my first time that I learn python And ")
+print("it`s difficult for me as I don`t even know basics but Thanks to Atul Sir, for")
+print("giving me the knowledge of Python and GitHub also. We all know python is not that ")
+print("easy which can be learned in only 10 days, But now i know the bascis & know how to ")
+print("learn learn any programming language not only python. Sir your teaching in a ")
+print("very excellent way...Not only teaching, you motivates and inspires each and ")
+print("individual student i all aspects always with smiling face ....")
+print("Thank you so mach for spending your valuable time with us.\n")
+
+print("THANK YOU ATUL SIR! ")
